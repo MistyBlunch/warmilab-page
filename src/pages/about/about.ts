0dbf9c0c3add221617a2eb50019ec572ce5659fc
MistyBlunch <gracenikole@hotmail.com>
2017-11-18 12:06:09 -0500

    Agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,37 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+  nombre: string = "Grace Nikole";
+  nickname: string = "Misty Blunch";
+  edad: number = 16;
+
+
+  datos: any = {
+    nombre: "Grace Nikole",
+    nickname: "Misty Blunch",
+    edad: 16
+  };
+
+  origen: any ={
+    titulo: "Origen",
+    dato: "Lima, Peru"
+  };
+
+  email: any ={
+    titulo: "E-mail",
+    dato: "gracenikole@hotmailcom"
+  };
+
+  phone: any ={
+    titulo: "Phone",
+    dato: 959851104
+  };
+
+  btc: any ={
+    titulo: "BTC address",
+    dato: "sSDI993nldioHIOW53hf"
+  };
+
   constructor(public navCtrl: NavController) {
 
   }
